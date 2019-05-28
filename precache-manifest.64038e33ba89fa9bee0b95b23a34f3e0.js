60482af269ca9e0eb2d7a50e16d4008a7caee14b
Marcin Olszowski <olszyna38@gmail.com>
2019-05-28 15:59:28 +0200

    Updates

---
+++ precache-manifest.64038e33ba89fa9bee0b95b23a34f3e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e4616dce6a2c5d8e03ec3e054dfadf",
+    "url": "/sudoku_plus/index.html"
+  },
+  {
+    "revision": "6173d0bac590b574b5c6",
+    "url": "/sudoku_plus/static/css/main.3d5e61bc.chunk.css"
+  },
+  {
+    "revision": "e8bfa6d65cebddec4d48",
+    "url": "/sudoku_plus/static/js/2.e95650df.chunk.js"
+  },
+  {
+    "revision": "6173d0bac590b574b5c6",
+    "url": "/sudoku_plus/static/js/main.d15c6b5c.chunk.js"
+  },
+  {
+    "revision": "5e421717538f78498e26",
+    "url": "/sudoku_plus/static/js/runtime~main.5a56425c.js"
+  }
+]);
